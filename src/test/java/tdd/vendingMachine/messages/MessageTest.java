bac86e1d136c346f1372c50ac575c712267bd941
Daniel Wąsik <daniel.wasik1@gmail.com>
2016-10-13 03:11:09 +0200

    unit tests for Messages

--- src/test/java/tdd/vendingMachine/messages/MessageTest.java
+++ src/test/java/tdd/vendingMachine/messages/MessageTest.java
@@ -1,61 +1,40 @@
 package tdd.vendingMachine.messages;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.junit.runners.Parameterized;
-import org.junit.runner.RunWith;
+
+import tdd.vendingMachine.impl.messages.ERROR;
+import tdd.vendingMachine.impl.messages.INFORMATION;
+import tdd.vendingMachine.impl.messages.WARNING;
 
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
 @FixMethodOrder(MethodSorters.JVM)
 public class MessageTest {
 
-    public MESSAGE_ENUM_DUMMY messageType;
-
-    public MessageTest(MESSAGE_ENUM_DUMMY messageType) {
-        this.messageType = messageType;
-    }
-
     @Test
     public final void testClassObjectNotNull() {
-        assertNotNull(messageType);
+        assertNotNull(INFORMATION.ALLOWED_COINS);
+        assertNotNull(WARNING.NO_CHANGE);
+        assertNotNull(ERROR.WRONG_INIT_CONFIG);
     }
 
     @Test
     public final void testInformationMessage() {
         String msg = "You can insert such coins: ";
-        assertTrue(msg.compareTo(messageType.get()) == 0);
+        assertTrue(msg.compareTo(INFORMATION.ALLOWED_COINS.get()) == 0);
     }
 
     @Test
     public final void testWarningMessage() {
         String msg = "Sorry, there is no enough coins to give you a change. Please, get back your money.";
-        assertTrue(msg.compareTo(messageType.get()) == 0);
+        assertTrue(msg.compareTo(WARNING.NO_CHANGE.get()) == 0);
     }
 
     @Test
     public final void testErrorMessage() {
         String msg = "Vending machine was not configured properly.";
-        assertTrue(msg.compareTo(messageType.get()) == 0);
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object> instancesToTest() {
-        return Arrays.asList(new Object[]{
-                                            new MESSAGE_ENUM_DUMMY()
-                                            });
-    }
-}
-
-class MESSAGE_ENUM_DUMMY {
-    static private String msg = "";
-
-    public String get() {
-        return msg;
+        assertTrue(msg.compareTo(ERROR.WRONG_INIT_CONFIG.get()) == 0);
     }
 }
